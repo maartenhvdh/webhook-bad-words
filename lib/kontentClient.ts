62d62ee0cd5472863015f47bd70085b4a00b2b01
Maarten van den Hooven <maartenv@kontent.ai>
2023-01-09 14:26:06 +0100

    update webhook project

--- lib/kontentClient.ts
+++ lib/kontentClient.ts
@@ -1,10 +1,7 @@
 // Kentico Kontent Delivery API
-
-import { createDeliveryClient, createRichTextHtmlResolver, linkedItemsHelper, camelCasePropertyNameResolver } from '@kontent-ai/delivery-sdk'
+import { createDeliveryClient, camelCasePropertyNameResolver } from '@kontent-ai/delivery-sdk'
 import { name, version } from '../package.json'
 import { BasicPage } from "../models/content-types/basic_page"
-import { RichTextElement } from '@kontent-ai/react-components';
-import { GlobalBranding } from '../models/content-types/global_branding';
 
 const sourceTrackingHeaderName = 'X-KC-SOURCE'
 
@@ -23,27 +20,6 @@ const client = createDeliveryClient({
   propertyNameResolver: camelCasePropertyNameResolver
 });
 
-/* Global Branding */
-
-export async function getGlobalBrandingItem() : Promise<GlobalBranding> {
-  const response = await client
-  .item("global_branding")
-  .toPromise();
-
-  return (response.data.item as GlobalBranding);
-}
-
-/* Basic Page */
-
-export async function getBasicPageItem(itemCodeName) : Promise<BasicPage> {
-  const response = await client
-    .item(itemCodeName)
-    .toPromise();
-
-    return (response.data.item as BasicPage);
-}
-
-
 export async function getBasicPageItemById(itemId) : Promise<BasicPage[]> {
   const response = await client
     .items<BasicPage>()
@@ -53,26 +29,3 @@ export async function getBasicPageItemById(itemId) : Promise<BasicPage[]> {
 
     return response.data.items;
 }
-
-
-export async function getBasicPageItemList() : Promise<BasicPage[]> {
-  const response = await client
-    .items<BasicPage>()
-    .type('basic_page')
-    //.elementsParameter(['report_name', 'code_name', 'url_slug', 'publication_type', 'financial_year', 'audience'])
-    .toAllPromise();
-
-    return response.data.items;
-}
-
-
-export async function getBasicPageItemCodenameList() : Promise<BasicPage[]> {
-  const response = await client
-    .items<BasicPage>()
-    .type('basic_page')
-    .elementsParameter(['code_name'])
-    .toAllPromise();
-
-    return response.data.items;
-}
-
